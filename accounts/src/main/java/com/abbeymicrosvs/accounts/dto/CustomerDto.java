d1037aae4450dfc8940c0c5f1589965dc72980ea
Abbey <abbeylincholn4real@gmail.com>
2025-01-13 11:36:20 +0000

    Validations and Exceptions

--- accounts/src/main/java/com/abbeymicrosvs/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/abbeymicrosvs/accounts/dto/CustomerDto.java
@@ -1,17 +1,29 @@
 package com.abbeymicrosvs.accounts.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name should not be null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer nae should be between 5 and 30 characters")
     private String name;
 
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotEmpty(message = "Mobile number should not be null or empty")
+    @Pattern(regexp = "(^$|^[0-9]{10})", message = "Mobile number must be 10 digits")
 
     private String mobileNumber;
 
+    @NotEmpty
     private AccountsDto accountsDto;
 
 }
--- accounts/src/main/java/com/abbeymicrosvs/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/abbeymicrosvs/accounts/exception/GlobalExceptionHandler.java
@@ -1,16 +1,27 @@
 package com.abbeymicrosvs.accounts.exception;
 
 import com.abbeymicrosvs.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+    }
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
@@ -22,6 +33,19 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                            WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
