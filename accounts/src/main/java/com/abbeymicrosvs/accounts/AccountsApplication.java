c499a80612fd1735aec887b42c6394f246e4f9c8
Abbey <abbeylincholn4real@gmail.com>
2025-01-15 15:58:58 +0000

    Merge branch 'REST-API-documentation' of https://github.com/abbeylincholn/Microservices-SpringBoot- into REST-API-documentation

--- accounts/src/main/java/com/abbeymicrosvs/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/abbeymicrosvs/accounts/AccountsApplication.java
@@ -1,11 +1,37 @@
 package com.abbeymicrosvs.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "AbbeyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Boss Oddy",
+						email = "oddy@cst.technology",
+						url = "https://cst.technology"
+				),
+				license = @License(
+					name = "Apache 2.0",
+					url = "https://cst.technology"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "AbbeyBank Accounts microservice REST API Documentation",
+				url = "https://cst.technology"
+		)
+)
+
 
 public class AccountsApplication {
 
