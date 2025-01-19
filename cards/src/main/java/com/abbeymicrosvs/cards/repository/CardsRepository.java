f28b5289845e31ee80e26ea3e1c05f68a4f1dab2
Abbey <abbeylincholn4real@gmail.com>
2025-01-19 13:29:31 +0000

    Card works

--- cards/src/main/java/com/abbeymicrosvs/cards/repository/CardsRepository.java
+++ cards/src/main/java/com/abbeymicrosvs/cards/repository/CardsRepository.java
@@ -6,9 +6,15 @@ import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 
 public interface CardsRepository extends JpaRepository<Cards, Long> {
     // this will take the entity classes and interact with the database table created
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
 
 }
